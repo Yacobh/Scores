7014725eaf01a8e64059825bf9f8c1a4bbfb9142
yacobh <jacobocordova@gmail.com>
2020-02-01 23:42:17 -0300

    Initial Commit

---
+++ Scores/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Scores
+//
+//  Created by Yacobh on 2/1/20.
+//  Copyright © 2020 Yacobh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
